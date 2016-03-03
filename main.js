ea10b7fb14ea1ca2d1a9e1f38ed1eb62fec7ebf0
Guido Bakkes <gbakkes@gmail.com>
2016-03-03 13:58:52 +0100

    Draw lines instead of dots

--- main.js
+++ main.js
@@ -10,20 +10,28 @@ var dragging = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+context.lineWidth = radius * 2;
+
 var putPoint = function(e) {
     if (dragging) {
+        context.lineTo(e.clientX, e.clientY);
+        context.stroke();
         context.beginPath();
         context.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2)
         context.fill();
+        context.beginPath();
+        context.moveTo(e.clientX, e.clientY);
     }
 }
 
-var engage = function() {
+var engage = function(e) {
     dragging = true;
+    putPoint(e);
 }
 
 var disengage = function() {
     dragging = false;
+    context.beginPath();
 }
 canvas.addEventListener('mousedown', engage);
 canvas.addEventListener('mousemove', putPoint);
