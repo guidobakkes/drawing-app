07d7b702d3ff513102543272479a06c976b511d2
Guido Bakkes <gbakkes@gmail.com>
2016-03-03 12:21:03 +0100

    Initial commit
    Add canvas

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by guidobakkes on 03/03/16.
+ */
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+var radius = 10;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var putPoint = function(e) {
+    context.beginPath();
+    context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2)
+    context.fill();
+}
+
+canvas.addEventListener('mousedown', putPoint);
